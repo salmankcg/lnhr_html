9640bd865f54e070943fd914d99b518341a764ae
kmasalam <mccyber007@gmail.com>
2022-03-02 20:27:07 +0600

    image before after and testimonial done
    Image before after and testimonial done . please don't use procedure and products now

--- assets/js/main.js
+++ assets/js/main.js
@@ -33,18 +33,76 @@
         })
 
         //
+        // var swiper = new Swiper(".lnhr_new_banner__slider", {
+        //     pagination: {
+        //         el: ".swiper-pagination",
+        //     },
+        //     clickable: true,
+        // });
+
         var swiper = new Swiper(".lnhr_new_banner__slider", {
-            pagination: {
-            el: ".swiper-pagination",
+            effect: "fade",
+            slidesPerView: 1,
+            autoplay: {
+                delay: 2500,
+                disableOnInteraction: false,
             },
-        });
+            pagination: {
+              el: ".swiper-pagination",
+              clickable: true,
+              renderBullet: function (index, className) {
+                return '<span class="' + className + '">' + (index + 1) + "</span>";
+              },
+            },
+            
+          });
+
+          // Testimonial Slider
+          var swiper = new Swiper(".lnhr_new_testimonial__slider", {
+            // effect: "fade",
+            slidesPerView: 1,
+            // autoplay: {
+            //     delay: 2500,
+            //     disableOnInteraction: false,
+            // },
+            navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+            },
+            pagination: {
+              el: ".swiper-pagination",
+              clickable: true,
+              renderBullet: function (index, className) {
+                return '<span class="' + className + '">' + "</span>";
+              },
+            },
+            
+          });
 
         
 
         $(window).on('load resize', function () {
             mobileAccordion()
         });
-       
+
+        
+       //
+       var wow = new WOW(
+        {
+          boxClass:     'wow',      // animated element css class (default is wow)
+          animateClass: 'animated', // animation css class (default is animated)
+          offset:       0,          // distance to the element when triggering the animation (default is 0)
+          mobile:       true,       // trigger animations on mobile devices (default is true)
+          live:         true,       // act on asynchronously loaded content (default is true)
+          callback:     function(box) {
+            // the callback is fired every time an animation is started
+            // the argument that is passed in is the DOM node being animated
+          },
+          scrollContainer: null,    // optional scroll container selector, otherwise use window,
+          resetAnimation: true,     // reset animation on end (default is true)
+        }
+      );
+      wow.init();
 
 
         
@@ -74,5 +132,19 @@
 
 
 	});
+
+
+    //
+
+    $(window).load(function() {
+
+        $('.lnhr_new_image_compare').cross2({
+            clickEnabled: true,
+            easing: 'easeInBack',
+            animationDuration: 500,
+            mousewheelEnabled: false,
+            titlesEnabled:false,
+        });
+    });
 
 })(jQuery)
