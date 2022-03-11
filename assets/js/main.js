c405d5276b5d6288bef4e56b0f59ec56f0e7a1eb
kmasalam <mccyber007@gmail.com>
2022-03-11 21:59:30 +0600

    footer and navigation issue fixed

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,7 +12,7 @@
   // Mobile Menu
   $(document).ready(function () {
 
- 
+    mobileAccordion();
     //
     const lnhr_new_navbar__nav_li = $(".lnhr_new_navbar__nav li");
     lnhr_new_navbar__nav_li.each(function () {
@@ -25,7 +25,7 @@
     if(classExist('.lnhr_new_navbar')){
         $(".lnhr_new_navbar").meanmenu({
             meanScreenWidth: "1200",
-            meanMenuOpen: `<span class="lnhr_new_menu_clicker"><span class="lnhr_new_menu_text">Menu</span> <img class="lnhr_new_menu_img" src="assets/images/mobile_menu_bar.svg" alt="menu"></span>`,
+            meanMenuOpen: `<span class="lnhr_new_menu_clicker"> <span class="lnhr_new_menu_text">Menu</span> <img class="lnhr_new_menu_img" src="assets/images/mobile_menu_bar.svg" alt="menu"></span>`,
             meanMenuClose: `<span class="lnhr_new_menu_clicker"><span class="lnhr_new_menu_text">Close</span> <span class="cl_icon">X</span></span>`,
             meanRevealColour: "tansparent",
           });
@@ -104,7 +104,7 @@
       }
 
 
-    $(window).on("load resize", function () {
+    $(window).on("resize", function () {
       mobileAccordion();
     });
 
@@ -128,54 +128,64 @@
 
     //
 
-    // Footer Accordion
-    function mobileAccordion() {
-      if (window.innerWidth < 768) {
-        let accordionWidget = $(".lnhr_new_footer__widget");
-        let accordionTitle = $(".lnhr_new_footer__widget_title");
-        let accordionContent = $(".lnhr_new_footer__widget_content");
-        accordionWidget
-          .not(".lnhr_new_footer_social_widget")
-          .addClass("lnhr_new_accordion_off");
-        accordionWidget
-          .not(".lnhr_new_footer_social_widget")
-          .find(accordionContent)
-          .slideUp();
-        accordionTitle.on("click", function (e) {
-          e.preventDefault();
-          let __this = $(this);
-          $(this)
-            .parent(accordionWidget)
-            .stop()
-            .toggleClass("lnhr_new_accordion_off");
-          $(this)
-            .parent(accordionWidget)
-            .stop()
-            .toggleClass("lnhr_new_accordion_on");
-          $(this).siblings(accordionContent).stop().slideToggle();
-          $(this)
-            .parents(".lnhr_ac_parent")
-            .siblings(".lnhr_ac_parent")
-            .find(accordionWidget)
-            .stop()
-            .removeClass("lnhr_new_accordion_on");
-          $(this)
-            .parents(".lnhr_ac_parent")
-            .siblings(".lnhr_ac_parent")
-            .find(accordionContent)
-            .stop()
-            .slideUp();
-        });
-      }
-    }
+    
   });
 
   //
+  // Footer Accordion
+  function mobileAccordion() {
+    if (window.innerWidth < 768) {
+      let accordionWidget = $(".lnhr_new_footer__widget");
+      let accordionTitle = $(".lnhr_new_footer__widget_title");
+      let accordionContent = $(".lnhr_new_footer__widget_content");
+      accordionWidget
+        .not(".lnhr_new_footer_social_widget")
+        .addClass("lnhr_new_accordion_off");
+      accordionWidget
+        .not(".lnhr_new_footer_social_widget")
+        .find(accordionContent)
+        .slideUp();
+
+        $('.lnhr_ac_parent').each(function(e){
+            if(!$(this).find('.lnhr_new_footer__widget_content').length){
+                $(this).addClass('lnhr_disable_accordion')
+            }
+        })
+      
+      accordionTitle.on("click touchend", function (e) {
+        
+        e.preventDefault();
+        let __this = $(this);
+        $(this)
+          .parent(accordionWidget)
+          .toggleClass("lnhr_new_accordion_off");
+        $(this)
+          .parent(accordionWidget)
+          .toggleClass("lnhr_new_accordion_on");
+        $(this).siblings(accordionContent).stop().slideToggle();
+        $(this)
+          .parents(".lnhr_ac_parent")
+          .siblings(".lnhr_ac_parent")
+          .find(accordionWidget)
+          .removeClass("lnhr_new_accordion_on");
+        $(this)
+          .parents(".lnhr_ac_parent")
+          .siblings(".lnhr_ac_parent")
+          .find(accordionContent)
+          .slideUp();
+      });
+    }
+  }
 
   $(window).on('load',function () {
+    //
+    //mobileAccordion()
+    //
     $(".lnhr_new_image_compare_loaded").imagesLoaded(function () {
       $(".lnhr_new_image_compare").twentytwenty();
       });
     
   });
+
+
 })(jQuery);
