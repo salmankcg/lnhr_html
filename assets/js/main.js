50fec7a2059b050eb708bf893cc3c5328745ed63
kmasalam <mccyber007@gmail.com>
2022-03-21 21:48:58 +0600

    banner text alignment issue for russel vai

--- assets/js/main.js
+++ assets/js/main.js
@@ -51,10 +51,10 @@
         effect: "fade",
         slidesPerView: 1,
         loop: true,
-        autoplay: {
-          delay: 2500,
-          disableOnInteraction: false,
-        },
+        // autoplay: {
+        //   delay: 2500,
+        //   disableOnInteraction: false,
+        // },
         pagination: {
           el: ".swiper-pagination",
           clickable: true,
